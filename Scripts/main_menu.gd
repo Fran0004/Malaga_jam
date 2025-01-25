2df6c25b4d7bafa97f2580c5d0a85e06556987a4
Jose Antonio Perez Gonzalez <114741595+Jpergon251@users.noreply.github.com>
2025-01-25 22:57:28 +0100

    Animacion logo corregida

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,8 +1,10 @@
 extends Control
 
 
+@onready var menu_animator: AnimationPlayer = $MenuAnimator
 
-
+func _ready() -> void:
+	menu_animator.play("Logo")
 
 func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/game_scene.tscn")
