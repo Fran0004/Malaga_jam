2ae0b3a28517dfc4b826152a6f8b6908c99457b6
Jose Antonio Perez Gonzalez <114741595+Jpergon251@users.noreply.github.com>
2025-01-25 11:14:54 +0100

    Hechas funciones del player

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -1,38 +1,38 @@
 extends CharacterBody2D
 
-# Variables del glóbulo rojo
-@export var max_oxygen: int = 3  # Capacidad máxima de oxígeno
-var oxygen_carried: int = 0  # Oxígeno que lleva actualmente
+@export var SPEED = 500.0  # Velocidad del personaje
+@onready var action_key: Sprite2D = $ActionKey
 
-@export var base_speed: float = 300.0  # Velocidad base
-var current_speed: float = base_speed  # Velocidad actual (modificable por buffs)
 
-func _physics_process(delta: float):
-	# Movimiento básico con WASD
-	var velocity = Vector2.ZERO
-	if Input.is_action_pressed("move_up"):
-		velocity.y -= current_speed
+func _physics_process(delta: float) -> void:
+	# Reiniciamos la velocidad en cada frame
+	velocity = Vector2.ZERO
+
+	if GameManager.key_sprite_show:
+		action_key.visible = true
+	else:
+		action_key.visible = false
+	# Detectar las teclas presionadas y ajustar la velocidad
 	if Input.is_action_pressed("move_down"):
-		velocity.y += current_speed
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= current_speed
+		velocity.y = SPEED
+	if Input.is_action_pressed("move_up"):
+		velocity.y = -SPEED
 	if Input.is_action_pressed("move_right"):
-		velocity.x += current_speed
+		velocity.x = SPEED
+	if Input.is_action_pressed("move_left"):
+		velocity.x = -SPEED
 
-	# Normalizamos para evitar movimiento más rápido en diagonal
-	if velocity != Vector2.ZERO:
-		velocity = velocity.normalized() * current_speed
 
+	#Interacciones personaje
+	if Input.is_action_just_pressed("trade"):
+		GameManager.trade_organ()
+	# Movemos el personaje
 	move_and_slide()
 
-# Recoger oxígeno
-func collect_oxygen(amount: int) -> void:
-	oxygen_carried += amount
-	oxygen_carried = clamp(oxygen_carried, 0, max_oxygen)
-	print("Oxígeno transportado: ", oxygen_carried)
 
-# Entregar oxígeno a un órgano
-func deliver_oxygen(organ) -> void:
-	if oxygen_carried > 0:
-		organ.receive_oxygen(oxygen_carried)
-		oxygen_carried = 0
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	GameManager.key_sprite_show = true
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	GameManager.key_sprite_show = false
